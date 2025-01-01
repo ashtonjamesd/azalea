9b80b941f47630cb054151aa317625e55bac4510
rxgq <ashtondunderdale@gmail.com>
2025-01-01 13:08:54 +0000

    initial commit

--- out/output.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-
-int main(int argc,char*argv[]){
-int x=10;
-printf("%s", x);
-return 0;
-}
--- src/main.c
+++ src/main.c
@@ -1,47 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "lexer.h"
-#include "parser.h"
-#include "ast.h"
-#include "transpiler.h"
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Usage: ./main.exe <path>");
-        return 1;
-    }
-
-    FILE *fptr = fopen(argv[1], "r");
-    if (!fptr) {
-        perror("File path was not found");
-        return 1;
-    }
-
-    fseek(fptr, 0, SEEK_END);
-    long sz = ftell(fptr);
-    rewind(fptr);
-
-    char *buffer = (char *)malloc(sz + 1);
-    if (!buffer) {
-        perror("Memory allocation failed");
-        fclose(fptr);
-        return 1;
-    }
-
-    size_t read_size = fread(buffer, 1, sz, fptr);
-    buffer[read_size] = '\0';
-    fclose(fptr);
-
-    Lexer *lexer = init_lexer(buffer);
-    Token* tokens = tokenize(lexer);
-    print_lexer(lexer);
-
-    Parser *parser = init_parser(tokens);
-    Program* ast = parse_ast(parser);
-    print_ast(ast);
-
-    generate(ast);
-
-    free(buffer);
+int main() {
+    printf("Hello, World!");
     return 0;
 }
